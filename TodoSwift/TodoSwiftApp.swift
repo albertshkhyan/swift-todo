029af200436563783e00c6a4e0a1dc0296b6ddf9
albertshkhyan <alikshkhyan@gmail.com>
2025-01-31 01:19:50 +0400

    Initial Commit

---
+++ TodoSwift/TodoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftApp.swift
+//  TodoSwift
+//
+//  Created by Albert Shkhyan on 31.01.25.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoSwiftTests/TodoSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  TodoSwiftTests.swift
+//  TodoSwiftTests
+//
+//  Created by Albert Shkhyan on 31.01.25.
+//
+
+import Testing
+
+struct TodoSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
